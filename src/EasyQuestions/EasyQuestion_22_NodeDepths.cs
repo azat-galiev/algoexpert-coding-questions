9749a81b2cb2d387d891282e876a1b32e9b16eea
Azat Galiev <v-azatgaliev@microsoft.com>
2023-07-17 21:49:03 +0200

    Day 6

---
+++ src/EasyQuestions/EasyQuestion_22_NodeDepths.cs
@@ -0,0 +1,70 @@
+﻿namespace CodingQuestions.EasyQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/node-depths"/>
+    /// Time: O(n)
+    /// Space: O(log(n)) in the best case (if tree is balanced), O(n) in the worst case
+    internal class EasyQuestion_22_NodeDepths : CodingQuestion<EasyQuestion_22_NodeDepths.BinaryTree, int>
+    {
+        protected override int ExecuteSolution(BinaryTree root)
+        {
+            var result = 0;
+            SearchDepthFirst(root, 0, ref result);
+            return result;
+        }
+
+        private void SearchDepthFirst(BinaryTree node, int currentDepth, ref int result)
+        {
+            result += currentDepth;
+
+            if (node.left != null)
+            {
+                SearchDepthFirst(node.left, currentDepth + 1, ref result);
+            }
+
+            if (node.right != null)
+            {
+                SearchDepthFirst(node.right, currentDepth + 1, ref result);
+            }
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int expected, int actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<BinaryTree, int> GetTests()
+        {
+            var root = new BinaryTree(1);
+            root.left = new BinaryTree(2);
+            root.right = new BinaryTree(3);
+
+            root.left.left = new BinaryTree(4);
+            root.left.right = new BinaryTree(5);
+
+            root.left.left.left = new BinaryTree(8);
+            root.left.left.right = new BinaryTree(9);
+
+            root.right.left = new BinaryTree(6);
+            root.right.right = new BinaryTree(7);
+
+            return new Dictionary<BinaryTree, int>
+            {
+                { root, 16 },
+            };
+        }
+
+        internal class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+                left = null;
+                right = null;
+            }
+        }
+    }
+}
---
+++ src/EasyQuestions/EasyQuestion_23_FindClosestValueInBST.cs
@@ -0,0 +1,82 @@
+﻿namespace CodingQuestions.EasyQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/find-closest-value-in-bst"/>
+    /// Time: O(log(n)) on average, O(n) in the worst case
+    /// Space: O(1)
+    internal class EasyQuestion_23_FindClosestValueInBST : CodingQuestion<EasyQuestion_23_FindClosestValueInBST.Input, int>
+    {
+        protected override int ExecuteSolution(Input input)
+        {
+            var currentNode = input.Tree;
+            var closest = input.Tree.value;
+            while (currentNode != null)
+            {
+                // if current closest value is further than the current node's value from the target, we update our closest
+                if (Math.Abs(input.Target - closest) > Math.Abs(input.Target - currentNode.value))
+                {
+                    closest = currentNode.value;
+                }
+
+                if (input.Target < currentNode.value)
+                {
+                    currentNode = currentNode.left;
+                }
+                else if (input.Target > currentNode.value)
+                {
+                    currentNode = currentNode.right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return closest;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int expected, int actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<Input, int> GetTests()
+        {
+            var root = new BST(10);
+            root.left = new BST(5);
+            root.right = new BST(15);
+
+            root.left.left = new BST(2);
+            root.left.right = new BST(5);
+
+            root.left.left.left = new BST(1);
+
+            root.right.left = new BST(13);
+            root.right.right = new BST(22);
+
+            root.right.left.right = new BST(14);
+
+            return new Dictionary<Input, int>
+            {
+                { new Input { Tree = root, Target = 12 }, 13 }
+            };
+        }
+
+        public class BST
+        {
+            public int value;
+            public BST left;
+            public BST right;
+
+            public BST(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        internal class Input
+        {
+            public BST Tree { get; set; }
+            public int Target { get; set; }
+        }
+    }
+}
