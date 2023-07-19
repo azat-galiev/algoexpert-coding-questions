2aa1058aeb2a932fe1fbc82e26083b17cb32c29a
Azat Galiev <v-azatgaliev@microsoft.com>
2023-07-19 22:28:58 +0200

    Day 8

---
+++ src/EasyQuestions/EasyQuestion_26_NthFibonacci.cs
@@ -0,0 +1,44 @@
+﻿namespace CodingQuestions.EasyQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/nth-fibonacci"/>
+    /// Time: O(n)
+    /// Space: O(1)
+    internal class EasyQuestion_26_NthFibonacci : CodingQuestion<int, int>
+    {
+        protected override int ExecuteSolution(int n)
+        {
+            if (n == 1) return 0;
+            if (n == 2) return 1;
+
+            var nMinus1 = 1;
+            var nMinus2 = 0;
+            var result = 0;
+            for (var i = 3; i <= n; i++)
+            {
+                result = nMinus1 + nMinus2;
+                nMinus2 = nMinus1;
+                nMinus1 = result;
+            }
+
+            return result;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int expected, int actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<int, int> GetTests()
+        {
+            return new Dictionary<int, int>
+            {
+                { 1, 0 },
+                { 2, 1 },
+                { 3, 1 },
+                { 4, 2 },
+                { 5, 3 },
+                { 6, 5 },
+            };
+        }
+    }
+}
---
+++ src/EasyQuestions/EasyQuestion_27_Find3LargestNumbers.cs
@@ -0,0 +1,67 @@
+﻿namespace CodingQuestions.EasyQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/find-three-largest-numbers"/>
+    /// Time: O(n)
+    /// Space: O(1)
+    internal class EasyQuestion_27_Find3LargestNumbers : CodingQuestion<int[], int[]>
+    {
+        protected override int[] ExecuteSolution(int[] array)
+        {
+            var largestNumbers = new int?[3];
+
+            // Iterating over input array
+            for (var i = 0; i < array.Length; i++)
+            {
+                // Iterating over the list of largest numbers to find if current number is larger than some of them
+                for (var j = 2; j >= 0; j--)
+                {
+                    // If we didn't store any number at all, just store it right away
+                    if (largestNumbers[j] == null)
+                    {
+                        largestNumbers[j] = array[i];
+                        break;
+                    }
+
+                    // If we did store something before, but it's smaller, then...
+                    if (largestNumbers[j] < array[i])
+                    {
+                        // Shift all the largest numbers to the left, to give new largest number the room
+                        for (var k = 1; k <= j; k++)
+                        {
+                            largestNumbers[k - 1] = largestNumbers[k];
+                        }
+
+                        largestNumbers[j] = array[i];
+                        break;
+                    }
+                }
+            }
+
+            return new int[]
+            {
+                largestNumbers[0].Value,
+                largestNumbers[1].Value,
+                largestNumbers[2].Value,
+            };
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int[] expected, int[] actual)
+        {
+            if (expected.Length != actual.Length) return false;
+            for (var i = 0; i < expected.Length; i++)
+            {
+                if (actual[i] != expected[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<int[], int[]> GetTests()
+        {
+            return new Dictionary<int[], int[]>
+            {
+                { new int[] { 141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7 }, new int[] { 18, 141, 541 } },
+            };
+        }
+    }
+}
