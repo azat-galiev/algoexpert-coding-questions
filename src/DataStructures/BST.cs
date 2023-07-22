33cb4702230e0faadda3677ee3851d4e0e1aa3c0
Azat Galiev <v-azatgaliev@microsoft.com>
2023-07-22 20:26:13 +0200

    Day 11

---
+++ src/DataStructures/BST.cs
@@ -0,0 +1,156 @@
+﻿namespace CodingQuestions.DataStructures
+{
+    /// <summary>
+    /// Binary Search Tree
+    /// </summary>
+    internal class BST : IEquatable<BST>
+    {
+        public int Value;
+        public BST? Left;
+        public BST? Right;
+
+        public bool IsLeaf => Left == null && Right == null;
+        public bool HasSingleChild => (Left != null && Right == null) || (Left == null && Right != null);
+
+        public BST(int value)
+        {
+            Value = value;
+        }
+
+        // Time: O(log(n)) on average, O(n) in the worst case
+        // Space: O(1)
+        public BST Insert(int value)
+        {
+            var currentNode = this;
+            while (true)
+            {
+                if (value >= currentNode.Value)
+                {
+                    if (currentNode.Right == null)
+                    {
+                        currentNode.Right = new BST(value);
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.Right;
+                    }
+                }
+                else
+                {
+                    if (currentNode.Left == null)
+                    {
+                        currentNode.Left = new BST(value);
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.Left;
+                    }
+                }
+            }
+
+            return this;
+        }
+
+        // Time: O(log(n)) on average, O(n) in the worst case
+        // Space: O(1)
+        public bool Contains(int value)
+        {
+            var currentNode = this;
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value) return true;
+                else if (value < currentNode.Value) currentNode = currentNode.Left;
+                else if (value > currentNode.Value) currentNode = currentNode.Right;
+            }
+
+            return false;
+        }
+
+        // Time: O(log(n)) on average, O(n) in the worst case
+        // Space: O(1)
+        public BST Remove(int value)
+        {
+            Remove(value, null);
+            return this;
+        }
+
+        private void Remove(int value, BST? parentNode)
+        {
+            var currentNode = this;
+            while (currentNode != null)
+            {
+                if (value < currentNode.Value)
+                {
+                    parentNode = currentNode;
+                    currentNode = currentNode.Left;
+                }
+                else if (value > currentNode.Value)
+                {
+                    parentNode = currentNode;
+                    currentNode = currentNode.Right;
+                }
+                else
+                {
+                    // We have both subtrees, replacing the current node with the minumum value from the right subtree
+                    if (currentNode.Left != null && currentNode.Right != null)
+                    {
+                        currentNode.Value = currentNode.Right.FindMinValue();
+                        currentNode.Right.Remove(currentNode.Value, currentNode);
+                    }
+                    // Removing root node without at least one subtree
+                    else if (parentNode == null)
+                    {
+                        // Making left node a new root
+                        if (currentNode.Left != null)
+                        {
+                            currentNode.Value = currentNode.Left.Value;
+                            currentNode.Right = currentNode.Left.Right;
+                            currentNode.Left = currentNode.Left.Left;
+                        }
+                        // Making right node a new root
+                        else if (currentNode.Right != null)
+                        {
+                            currentNode.Value = currentNode.Right.Value;
+                            currentNode.Left = currentNode.Right.Left;
+                            currentNode.Right = currentNode.Right.Right;
+                        }
+                        // Removing single root node
+                        else
+                        {
+                            // noop
+                        }
+                    }
+                    else if (parentNode.Left == currentNode)
+                    {
+                        parentNode.Left = currentNode.Left ?? currentNode.Right;
+                    }
+                    else if (parentNode.Right == currentNode)
+                    {
+                        parentNode.Right = currentNode.Left ?? currentNode.Right;
+                    }
+                    break;
+                }
+            }
+        }
+
+        private int FindMinValue()
+        {
+            var currentNode = this;
+            while (currentNode.Left != null) currentNode = currentNode.Left;
+
+            return currentNode.Value;
+        }
+
+        public bool Equals(BST? other)
+        {
+            if (other == this) return true;
+            if (other == null) return false;
+
+            return other.Value == Value &&
+                ((other.Left == null && Left == null) || (other.Left != null && Left != null && other.Left.Equals(Left))) &&
+                ((other.Right == null && Right == null) || (other.Right != null && Right != null && other.Right.Equals(Right)));
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_01_BSTConstruction.cs
@@ -0,0 +1,60 @@
+﻿using CodingQuestions.DataStructures;
+
+namespace CodingQuestions.MediumQuestions
+{
+    internal class MediumQuestion_01_BSTConstruction : CodingQuestion<MediumQuestion_01_BSTConstruction.Input, BST>
+    {
+        protected override BST ExecuteSolution(Input input)
+        {
+            /// Actual implementation is in the <see cref="BST"/> class.
+            return input.Action(input.BST);
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(BST expected, BST actual)
+        {
+            return expected.Equals(actual);
+        }
+
+        protected override IReadOnlyDictionary<Input, BST> GetTests()
+        {
+            var initialBST = new BST(10);
+            initialBST.Left = new BST(5);
+            initialBST.Left.Left = new BST(2);
+            initialBST.Left.Left.Left = new BST(1);
+            initialBST.Left.Right = new BST(5);
+            initialBST.Right = new BST(15);
+            initialBST.Right.Left = new BST(13);
+            initialBST.Right.Left.Right = new BST(14);
+            initialBST.Right.Right = new BST(22);
+
+            var expectedBST = new BST(12);
+            expectedBST.Left = new BST(5);
+            expectedBST.Left.Left = new BST(2);
+            expectedBST.Left.Left.Left = new BST(1);
+            expectedBST.Left.Right = new BST(5);
+            expectedBST.Right = new BST(15);
+            expectedBST.Right.Left = new BST(13);
+            expectedBST.Right.Left.Right = new BST(14);
+            expectedBST.Right.Right = new BST(22);
+
+            return new Dictionary<Input, BST>
+            {
+                {
+                    new Input
+                    {
+                        BST = initialBST,
+                        Action = bst => bst.Insert(12).Remove(10)
+                    },
+                    expectedBST
+                }
+            };
+        }
+
+        internal class Input
+        {
+            public required BST BST { get; set; }
+
+            public required Func<BST, BST> Action { get; set; }
+        }
+    }
+}
