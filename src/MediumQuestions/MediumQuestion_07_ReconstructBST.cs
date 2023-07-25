54c7d8a81a1011c2db59f6df387258b6732df6f2
Azat Galiev <v-azatgaliev@microsoft.com>
2023-07-25 21:13:12 +0200

    Day 14

---
+++ src/MediumQuestions/MediumQuestion_07_ReconstructBST.cs
@@ -0,0 +1,89 @@
+﻿using CodingQuestions.DataStructures;
+
+namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/reconstruct-bst"/>
+    /// Time: O(n)
+    /// Space: O(n)
+    internal class MediumQuestion_07_ReconstructBST : CodingQuestion<List<int>, BST>
+    {
+        protected override BST ExecuteSolution(List<int> values)
+        {
+            return ProcessRange(values, 0, values.Count);
+        }
+
+        private BST ProcessRange(List<int> values, int indexFrom, int indexTo)
+        {
+            // We try to find the index of the node to the right by skipping
+            // values smaller than the current one.
+            int? rightIndex = null;
+            for (var i = indexFrom + 1; i < indexTo; i++)
+            {
+                if (values[i] >= values[indexFrom])
+                {
+                    rightIndex = i;
+                    break;
+                }
+            }
+
+            // If we have the next node, and it's not the right node,
+            // we treat it as the left node.
+            int? leftIndex = null;
+            if ((rightIndex == null && indexFrom + 1 < indexTo) || (rightIndex != null && rightIndex.Value - indexFrom > 1))
+            {
+                leftIndex = indexFrom + 1;
+            }
+
+            // If we found left node's index, call the function recursively
+            // with all the numbers until the start of the right node, or the end.
+            BST leftNode = null;
+            if (leftIndex != null)
+            {
+                var leftLength = rightIndex == null ? indexTo : rightIndex.Value;
+                leftNode = ProcessRange(values, leftIndex.Value, leftLength);
+            }
+
+            // If we found right node's index, we treat all the numbers
+            // until the end as belonging to right subtree.
+            BST rightNode = null;
+            if (rightIndex != null)
+            {
+                rightNode = ProcessRange(values, rightIndex.Value, indexTo);
+            }
+
+            var node = new BST(values[indexFrom]);
+            node.Left = leftNode;
+            node.Right = rightNode;
+
+            return node;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(BST expected, BST actual)
+        {
+            return expected.Equals(actual);
+        }
+
+        protected override IReadOnlyDictionary<List<int>, BST> GetTests()
+        {
+            var root = new BST(10);
+            root.Left = new BST(4);
+            root.Right = new BST(17);
+
+            root.Left.Left = new BST(2);
+            root.Left.Right = new BST(5);
+
+            root.Left.Left.Left = new BST(1);
+
+            root.Right.Right = new BST(19);
+            root.Right.Right.Left = new BST(18);
+
+            return new Dictionary<List<int>, BST>
+            {
+                {
+                    new List<int> { 10, 4, 2, 1, 5, 17, 19, 18 },
+                    root
+                }
+            };
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_08_TaskAssignment.cs
@@ -0,0 +1,71 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/task-assignment"/>
+    /// Time: O(nlog(n))
+    /// Space: O(n)
+    internal class MediumQuestion_08_TaskAssignment : CodingQuestion<List<int>, List<List<int>>>
+    {
+        protected override List<List<int>> ExecuteSolution(List<int> tasks)
+        {
+            // We need to remember indicies before sorting the array.
+            var timeToIndex = new Dictionary<int, Stack<int>>();
+            for (var i = 0; i < tasks.Count; i++)
+            {
+                var time = tasks[i];
+
+                if (!timeToIndex.ContainsKey(time))
+                    timeToIndex[time] = new Stack<int>();
+
+                timeToIndex[time].Push(i);
+            }
+
+            tasks.Sort((a, b) => a - b);
+
+            // Make up pairs of quick and long tasks together.
+            var pairs = new List<List<int>>();
+            for (var i = 0; i < tasks.Count / 2; i++)
+            {
+                var quickTaskTime = tasks[i];
+                var longTaskTime = tasks[tasks.Count - 1 - i];
+
+                var quickTaskIndex = timeToIndex[quickTaskTime].Pop();
+                var longTaskIndex = timeToIndex[longTaskTime].Pop();
+
+                pairs.Add(new List<int> { quickTaskIndex, longTaskIndex });
+            }
+
+            return pairs;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(List<List<int>> expected, List<List<int>> actual)
+        {
+            if (expected.Count != actual.Count) return false;
+            for (var i = 0; i < expected.Count; i++)
+            {
+                if (expected[i].Count != actual[i].Count) return false;
+                for (var j = 0; j < expected[i].Count; j++)
+                {
+                    if (expected[i][j] != actual[i][j]) return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<List<int>, List<List<int>>> GetTests()
+        {
+            return new Dictionary<List<int>, List<List<int>>>
+            {
+                {
+                    new List<int> { 1, 3, 5, 3, 1, 4 },
+                    new List<List<int>>
+                    {
+                        new List<int> { 4, 2 },
+                        new List<int> { 0, 5 },
+                        new List<int> { 3, 1 },
+                    }
+                }
+            };
+        }
+    }
+}
