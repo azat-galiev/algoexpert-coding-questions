50602ede4ad1a0efc777056a8dc1be2795fe6fa0
Azat Galiev <v-azatgaliev@microsoft.com>
2023-07-26 23:04:26 +0200

    Day 15

---
+++ src/MediumQuestions/MediumQuestion_09_MinimumCharactersForWords.cs
@@ -0,0 +1,66 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/minimum-characters-for-words"/>
+    /// Time: O(n * l) where n is the number of words, l is the length of the longest word
+    /// Space: O(c) where c is the number of unique characters across all words
+    internal class MediumQuestion_09_MinimumCharactersForWords : CodingQuestion<string[], string[]>
+    {
+        protected override string[] ExecuteSolution(string[] words)
+        {
+            var charCounts = new Dictionary<char, int>();
+
+            // Let's iterate over words, and every char inside them.
+            foreach (var word in words)
+            {
+                var wordCharCounts = new Dictionary<char, int>();
+                foreach (var @char in word)
+                {
+                    if (!wordCharCounts.ContainsKey(@char)) wordCharCounts.Add(@char, 0);
+
+                    // We count how many times we saw the char in the current word.
+                    wordCharCounts[@char]++;
+
+                    // Then, if we didn't see this specific char so many times in other words, we record it.
+                    if (!charCounts.ContainsKey(@char) || charCounts[@char] < wordCharCounts[@char])
+                        charCounts[@char] = wordCharCounts[@char];
+                }
+            }
+
+            // Now, we have all the counts, we only need to convert it to the expected form.
+            var result = new List<string>();
+            foreach (var @char in charCounts.Keys)
+            {
+                var count = charCounts[@char];
+                for (var i = 0; i < count; i++) result.Add(@char.ToString());
+            }
+
+            return result.ToArray();
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(string[] expected, string[] actual)
+        {
+            if (expected.Length != actual.Length) return false;
+
+            Array.Sort(expected);
+            Array.Sort(actual);
+
+            for (var i = 0; i < expected.Length; i++)
+            {
+                if (expected[i] != actual[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<string[], string[]> GetTests()
+        {
+            return new Dictionary<string[], string[]>
+            {
+                {
+                    new string[] { "this", "that", "did", "deed", "them!", "a" },
+                    new string[] { "t", "t", "h", "i", "s", "a", "d", "d", "e", "e", "m", "!" }
+                }
+            };
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_10_NumberOfWaysToMakeChange.cs
@@ -0,0 +1,62 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/number-of-ways-to-make-change"/>
+    /// Time: O(c * a), where c is the number of coins, a is the target amount
+    /// Space: O(a)
+    internal class MediumQuestion_10_NumberOfWaysToMakeChange : CodingQuestion<MediumQuestion_10_NumberOfWaysToMakeChange.Input, int>
+    {
+        protected override int ExecuteSolution(Input input)
+        {
+            // Index in this array is the amount we want to get using the coins.
+            var amounts = new int[input.TargetAmount + 1];
+
+            // That's our base case: there's only a single way to get amount of 0 (not use any coins at all).
+            amounts[0] = 1;
+
+            // We'll use dynamic programming here.
+            // Let's spit the whole task into a set of simpler subtasks.
+            foreach (var coin in input.Coins)
+            {
+                for (var amount = 1; amount <= input.TargetAmount; amount++)
+                {
+                    // If the coin even bigger than the whole amount,
+                    // it's impossible to use it for this amount.
+                    if (coin > amount) continue;
+
+                    // If we need to make some amount using a given coin,
+                    // we can make it one way more than an amount smaller
+                    // than this amount by the size of the coin.
+                    amounts[amount] += amounts[amount - coin];
+                }
+            }
+
+            return amounts[input.TargetAmount];
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int expected, int actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<Input, int> GetTests()
+        {
+            return new Dictionary<Input, int>
+            {
+                {
+                    new Input
+                    {
+                        TargetAmount = 6,
+                        Coins = new int[] { 1, 5 }
+                    },
+                    2
+                }
+            };
+        }
+
+        internal class Input
+        {
+            public int TargetAmount { get; set; }
+            public required int[] Coins { get; set; }
+        }
+    }
+}
