33198615f516e858403488938b7694f14648abdb
Azat Galiev <v-azatgaliev@microsoft.com>
2023-07-27 20:58:41 +0200

    Day 16

---
+++ src/MediumQuestions/MediumQuestion_11_YoungestCommonAncestor.cs
@@ -0,0 +1,106 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/youngest-common-ancestor"/>
+    /// Time: O(d) where d is the maximum depth of two given descendants
+    /// Space: O(1)
+    internal class MediumQuestion_11_YoungestCommonAncestor
+        : CodingQuestion<MediumQuestion_11_YoungestCommonAncestor.Input, MediumQuestion_11_YoungestCommonAncestor.AncestralTree>
+    {
+        protected override AncestralTree ExecuteSolution(Input input)
+        {
+            var descendantOne = input.DescendantOne;
+            var descendantTwo = input.DescendantTwo;
+
+            var depthOne = GetDepth(descendantOne);
+            var depthTwo = GetDepth(descendantTwo);
+
+            // First, we make sure both descendants are at the same depth.
+            if (depthOne > depthTwo)
+            {
+                descendantOne = Move(descendantOne, depthOne - depthTwo);
+            }
+            else if (depthTwo > depthOne)
+            {
+                descendantTwo = Move(descendantTwo, depthTwo - depthOne);
+            }
+
+            // Now, we traverse upwards until we reach a common ancestor.
+            while (descendantOne != descendantTwo)
+            {
+                descendantOne = descendantOne!.Ancestor;
+                descendantTwo = descendantTwo!.Ancestor;
+            }
+
+            return descendantOne!;
+        }
+
+        // Calculate the depth of a given descendant.
+        // Time: O(d) where d is the depth of the given descendant
+        // Space: O(1)
+        private static int GetDepth(AncestralTree descendant)
+        {
+            var depth = 0;
+            while (descendant.Ancestor != null)
+            {
+                descendant = descendant.Ancestor;
+                depth++;
+            }
+
+            return depth;
+        }
+
+        // Return an ancestor of a given descendant in a given distance towards root.
+        // Time: O(d) where d is the distance to move towards root
+        // Space: O(1)
+        private static AncestralTree Move(AncestralTree descendant, int distance)
+        {
+            for (var i = 0; i < distance; i++)
+                descendant = descendant.Ancestor!;
+
+            return descendant;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(AncestralTree expected, AncestralTree actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<Input, AncestralTree> GetTests()
+        {
+            var a = new AncestralTree('A');
+            var b = new AncestralTree('B') { Ancestor = a };
+            var d = new AncestralTree('D') { Ancestor = b };
+            var e = new AncestralTree('E') { Ancestor = b };
+            var i = new AncestralTree('I') { Ancestor = d };
+
+            return new Dictionary<Input, AncestralTree>
+            {
+                {
+                    new Input
+                    {
+                        DescendantOne = e,
+                        DescendantTwo = i
+                    },
+                    b
+                }
+            };
+        }
+
+        internal class AncestralTree
+        {
+            public char Name;
+            public AncestralTree? Ancestor;
+
+            public AncestralTree(char name)
+            {
+                Name = name;
+            }
+        }
+
+        internal class Input
+        {
+            public required AncestralTree DescendantOne { get; set; }
+            public required AncestralTree DescendantTwo { get; set; }
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_12_MergeOverlappingIntervals.cs
@@ -0,0 +1,77 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/merge-overlapping-intervals"/>
+    /// Time: O(nlog(n)) where n is the number of input intervals
+    /// Space: O(n)
+    internal class MediumQuestion_12_MergeOverlappingIntervals : CodingQuestion<int[][], int[][]>
+    {
+        protected override int[][] ExecuteSolution(int[][] intervals)
+        {
+            // First, we sort intervals by their starts to allow sequential traversing through them.
+            Array.Sort(intervals, new IntervalComparer());
+
+            var result = new List<int[]>();
+            for (var i = 0; i < intervals.Length; i++)
+            {
+                // If we already have some interval in the result,
+                // we first check if we can expand it with the current interval.
+                // If we can, then we do it, otherwise we add it as a separate interval.
+                if (result.Count > 0 && result[result.Count - 1][1] >= intervals[i][0])
+                {
+                    result[result.Count - 1][1] = Math.Max(result[result.Count - 1][1], intervals[i][1]);
+                    continue;
+                }
+
+                result.Add(intervals[i]);
+            }
+
+            return result.ToArray();
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int[][] expected, int[][] actual)
+        {
+            if (expected.Length != actual.Length) return false;
+            for (var i = 0; i < expected.Length; i++)
+            {
+                if (expected[i].Length != actual[i].Length) return false;
+                for (var j = 0; j < expected[i].Length; j++)
+                {
+                    if (expected[i][j] != actual[i][j]) return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<int[][], int[][]> GetTests()
+        {
+            return new Dictionary<int[][], int[][]>
+            {
+                {
+                    new int[][]
+                    {
+                        new int[] { 1, 2, },
+                        new int[] { 3, 5, },
+                        new int[] { 4, 7, },
+                        new int[] { 6, 8, },
+                        new int[] { 9, 10, },
+                    },
+                    new int[][]
+                    {
+                        new int[] { 1, 2, },
+                        new int[] { 3, 8, },
+                        new int[] { 9, 10, },
+                    }
+                }
+            };
+        }
+
+        private class IntervalComparer : IComparer<int[]>
+        {
+            public int Compare(int[] a, int[] b)
+            {
+                return a[0] - b[0];
+            }
+        }
+    }
+}
