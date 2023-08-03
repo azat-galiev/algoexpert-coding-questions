a291e17fbe41dd0f7c63c8d530550f9e62eb5c5d
Azat Galiev <v-azatgaliev@microsoft.com>
2023-08-03 23:37:40 +0200

    Day 23

---
+++ src/MediumQuestions/MediumQuestion_25_SortStack.cs
@@ -0,0 +1,72 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/sort-stack"/>
+    /// Time: O(n^2) where n is the number of elements in the stack
+    /// Space: O(n)
+    internal class MediumQuestion_25_SortStack : CodingQuestion<List<int>, List<int>>
+    {
+        protected override List<int> ExecuteSolution(List<int> stack)
+        {
+            Sort(stack);
+            return stack;
+        }
+
+        private void Sort(List<int> stack)
+        {
+            // Empty stack is sorted stack :D
+            if (stack.Count == 0) return;
+
+            // So, in order to sort a non-empty stack, we remove the top,
+            // sort the remainder, and insert removed top back into the sorted stack,
+            // which is simpler.
+            var top = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+
+            Sort(stack);
+            Insert(stack, top);
+        }
+
+        private void Insert(List<int> stack, int valueToInsert)
+        {
+            // Here we have already sorted stack, or an empty one.
+            // So, if we see an element that is smaller than the current one, 
+            // then we can safely add our value after it.
+            if (stack.Count == 0 || stack[stack.Count - 1] <= valueToInsert)
+            {
+                stack.Add(valueToInsert);
+                return;
+            }
+
+            // The top of the sorted stack is bigger than the value to be added,
+            // so we need to remove some more elements before inserting.
+            // After that, we can insert removed top back to the stack.
+            var top = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+
+            Insert(stack, valueToInsert);
+            Insert(stack, top);
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(List<int> expected, List<int> actual)
+        {
+            if (expected.Count != actual.Count) return false;
+            for (var i = 0 ; i < actual.Count; i++)
+            {
+                if (actual[i] != expected[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<List<int>, List<int>> GetTests()
+        {
+            return new Dictionary<List<int>, List<int>>
+            {
+                {
+                    new List<int> { -5, 2, -2, 4, 3, 1 },
+                    new List<int> { -5, -2, 1, 2, 3, 4 }
+                }
+            };
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_26_PhoneNumberMnemonics.cs
@@ -0,0 +1,82 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/phone-number-mnemonics"/>
+    /// Time: O((4^n) * n) where n is the number of digits in a given phone number
+    /// Space: O(4^n)
+    /// 4 to the power of n because every digit might be map to up to 4 mnemonics.
+    /// Time is additionally multiplied by n because we have to copy strings all the time along the way.
+    internal class MediumQuestion_26_PhoneNumberMnemonics : CodingQuestion<string, List<string>>
+    {
+        protected override List<string> ExecuteSolution(string phoneNumber)
+        {
+            // We use dynamic programming here by splitting the problem into smaller problems.
+            // The simplest possible case is a mnemonic for an empty number, so we start with it.
+            var mnemonics = new List<string> { "" };
+
+            // With each next digit of the phone number, we're getting closer to the final problem.
+            foreach (var digit in phoneNumber)
+            {
+                var newMnemonics = new List<string>();
+
+                // Ok, we have a digit. We iterate through every mnemonic for that digit...
+                foreach (var digitMnemonic in map[digit])
+                {
+                    // ...and append its every mnemonic to all subproblems we solved before.
+                    foreach (var mnemonic in mnemonics)
+                    {
+                        newMnemonics.Add(mnemonic + digitMnemonic);
+                    }
+                }
+
+                mnemonics = newMnemonics;
+            }
+
+            return mnemonics;
+        }
+
+        private static readonly IReadOnlyDictionary<char, IEnumerable<char>> map = new Dictionary<char, IEnumerable<char>>
+        {
+            { '0', new char[] { '0' } },
+            { '1', new char[] { '1' } },
+            { '2', new char[] { 'a', 'b', 'c' } },
+            { '3', new char[] { 'd', 'e', 'f' } },
+            { '4', new char[] { 'g', 'h', 'i' } },
+            { '5', new char[] { 'j', 'k', 'l' } },
+            { '6', new char[] { 'm', 'n', 'o' } },
+            { '7', new char[] { 'p', 'q', 'r', 's' } },
+            { '8', new char[] { 't', 'u', 'v' } },
+            { '9', new char[] { 'w', 'x', 'y', 'z' } },
+        };
+
+        protected override bool CompareActualAndExpectedOuputs(List<string> expected, List<string> actual)
+        {
+            expected.Sort();
+            actual.Sort();
+
+            if (expected.Count != actual.Count) return false;
+            for (var i = 0; i < actual.Count; i++)
+            {
+                if (actual[i] != expected[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<string, List<string>> GetTests()
+        {
+            return new Dictionary<string, List<string>>
+            {
+                {
+                    "1905",
+                    new List<string>
+                    {
+                        "1w0j", "1w0k", "1w0l",
+                        "1x0j", "1x0k", "1x0l",
+                        "1y0j", "1y0k", "1y0l",
+                        "1z0j", "1z0k", "1z0l",
+                    }
+                }
+            };
+        }
+    }
+}
