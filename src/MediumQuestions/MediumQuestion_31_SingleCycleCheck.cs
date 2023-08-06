540bcf77a52211f0dffa836440e1c3857c577adf
Azat Galiev <v-azatgaliev@microsoft.com>
2023-08-06 17:06:42 +0200

    Day 26

---
+++ src/MediumQuestions/MediumQuestion_31_SingleCycleCheck.cs
@@ -0,0 +1,47 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/single-cycle-check"/>
+    /// Time: O(n) where n is the number of elements
+    /// Space: O(1)
+    internal class MediumQuestion_31_SingleCycleCheck : CodingQuestion<int[], bool>
+    {
+        protected override bool ExecuteSolution(int[] array)
+        {
+            // We'll keep track of how many times we jumped.
+            var jumps = 0;
+            var currentIndex = 0;
+
+            // If we jumped more times than there are elements,
+            // then we've got into an inner loop.
+            while (jumps < array.Length)
+            {
+                // If we reached the first element inside the while, it means that we didn't traverse through
+                // all the elements, otherwise while's condition wouldn't be satisfied.
+                if (currentIndex == 0 && jumps > 0) return false;
+
+                jumps++;
+
+                // Calculate the next element handling overflows.
+                currentIndex += array[currentIndex];
+                currentIndex %= array.Length;
+                if (currentIndex < 0)
+                    currentIndex += array.Length;
+            }
+
+            return currentIndex == 0;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(bool expected, bool actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<int[], bool> GetTests()
+        {
+            return new Dictionary<int[], bool>
+            {
+                { new int[] { 2, 3, 1, -4, -4, 2 }, true }
+            };
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_32_ArrayOfProducts.cs
@@ -0,0 +1,55 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/array-of-products"/>
+    /// Time: O(n) where n is the number of elements in the input array
+    /// Space: O(n)
+    internal class MediumQuestion_32_ArrayOfProducts : CodingQuestion<int[], int[]>
+    {
+        protected override int[] ExecuteSolution(int[] array)
+        {
+            var productToLeft = new int[array.Length];
+            var productToRight = new int[array.Length];
+            var result = new int[array.Length];
+
+            // It's always 1 at the edges, because there are no other elements to the left or right of them.
+            productToLeft[0] = 1;
+            productToRight[productToRight.Length - 1] = 1;
+
+            // Calculating the value of product to the left of every index.
+            for (var i = 1; i < array.Length; i++)
+                productToLeft[i] = productToLeft[i - 1] * array[i - 1];
+
+            // Calculating the value of product to the right of every index.
+            for (var i = array.Length - 2; i >= 0; i--)
+                productToRight[i] = productToRight[i + 1] * array[i + 1];
+
+            // Now, we calculate the final result.
+            for (var i = 0; i < array.Length; i++)
+                result[i] = productToLeft[i] * productToRight[i];
+
+            return result;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int[] expected, int[] actual)
+        {
+            if (expected.Length != actual.Length) return false;
+            for (var i = 0; i < expected.Length; i++)
+            {
+                if (expected[i] != actual[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<int[], int[]> GetTests()
+        {
+            return new Dictionary<int[], int[]>
+            {
+                {
+                    new int[] { 5, 1, 4, 2 },
+                    new int[] { 8, 40, 10, 20 }
+                }
+            };
+        }
+    }
+}
