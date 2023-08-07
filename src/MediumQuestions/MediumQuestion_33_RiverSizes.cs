554a8db653baa8e522b24fd74cd9948ae11dbe66
Azat Galiev <v-azatgaliev@microsoft.com>
2023-08-07 23:21:52 +0200

    Day 27

---
+++ src/MediumQuestions/MediumQuestion_33_RiverSizes.cs
@@ -0,0 +1,77 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/river-sizes"/>
+    /// Time: O(w * h) where w is the width of the matrix, h is the height of the matrix.
+    /// Space: O(r) where r is the length of all rivers (the number of 1s in the matrix)
+    internal class MediumQuestion_33_RiverSizes : CodingQuestion<int[,], List<int>>
+    {
+        protected override List<int> ExecuteSolution(int[,] matrix)
+        {
+            var seen = new HashSet<Tuple<int, int>>();
+            var result = new List<int>();
+
+            for (var row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (var col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 1 && !seen.Contains(new Tuple<int, int>(row, col)))
+                    {
+                        result.Add(GetRiverLength(matrix, seen, row, col));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        // Returns the length of the river at the given point. If it's not a river, returns 0.
+        private int GetRiverLength(int[,] matrix, HashSet<Tuple<int, int>> seen, int row, int col)
+        {
+            if (row < 0 || row >= matrix.GetLength(0)) return 0;
+            if (col < 0 || col >= matrix.GetLength(1)) return 0;
+            if (matrix[row, col] == 0) return 0;
+            if (seen.Contains(new Tuple<int, int>(row, col))) return 0;
+
+            seen.Add(new Tuple<int, int>(row, col));
+
+            return 1 +
+                GetRiverLength(matrix, seen, row + 1, col) +
+                GetRiverLength(matrix, seen, row - 1, col) +
+                GetRiverLength(matrix, seen, row, col + 1) +
+                GetRiverLength(matrix, seen, row, col - 1);
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(List<int> expected, List<int> actual)
+        {
+            if (expected.Count != actual.Count) return false;
+
+            expected.Sort();
+            actual.Sort();
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                if (expected[i] != actual[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<int[,], List<int>> GetTests()
+        {
+            return new Dictionary<int[,], List<int>>
+            {
+                {
+                    new int[,]
+                    {
+                        { 1, 0, 0, 1, 0 },
+                        { 1, 0, 1, 0, 0 },
+                        { 0, 0, 1, 0, 1 },
+                        { 1, 0, 1, 0, 1 },
+                        { 1, 0, 1, 1, 0 },
+                    },
+                    new List<int> { 1, 2, 2, 2, 5 }
+                },
+            };
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_34_ZeroSumSubarray.cs
@@ -0,0 +1,42 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/zero-sum-subarray"/>
+    /// Time: O(n) where n is the number of elements in the input array.
+    /// Space: O(n)
+    internal class MediumQuestion_34_ZeroSumSubarray : CodingQuestion<int[], bool>
+    {
+        protected override bool ExecuteSolution(int[] nums)
+        {
+            // We'll keep track of all the sums of subarrays starting from the 1st element.
+            var sums = new HashSet<int> { 0 };
+
+            var sum = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+
+                // If we already saw this value, it means that the sum of elements between
+                // the 1st occurrence (let's say at index x) and the current one (at index y)
+                // is 0, i.e. sum of elements between x and y is 0.
+                if (sums.Contains(sum)) return true;
+
+                sums.Add(sum);
+            }
+
+            return false;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(bool expected, bool actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<int[], bool> GetTests()
+        {
+            return new Dictionary<int[], bool>
+            {
+                { new int[] { -5, -5, 2, 3, -2}, true },
+            };
+        }
+    }
+}
