ae52e7855975fe21cfd43b77f5e02a49b7a8d644
Azat Galiev <v-azatgaliev@microsoft.com>
2023-08-08 19:48:49 +0200

    Day 28

---
+++ src/MediumQuestions/MediumQuestion_35_BreadthFirstSearch.cs
@@ -0,0 +1,76 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/breadth-first-search"/>
+    /// Time: O(v + e) where v is the number of vertices, e is the number of edges in a tree
+    /// Space: O(v)
+    internal class MediumQuestion_35_BreadthFirstSearch : CodingQuestion<MediumQuestion_35_BreadthFirstSearch.Node, List<string>>
+    {
+        protected override List<string> ExecuteSolution(Node root)
+        {
+            // We're using a queue to keep track of unvisited nodes.
+            // This will make sure they'll be traversed in breadth-first manner.
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            var result = new List<string>();
+
+            while (queue.TryDequeue(out var node))
+            {
+                result.Add(node.Name);
+
+                foreach (var child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(List<string> expected, List<string> actual)
+        {
+            if (expected.Count != actual.Count) return false;
+            for (var i = 0; i < expected.Count; i++)
+            {
+                if (expected[i] != actual[i]) return false;
+            }
+
+            return true;
+        }
+
+        protected override IReadOnlyDictionary<Node, List<string>> GetTests()
+        {
+            var root = new Node("A");
+            root.AddChild("B").AddChild("C").AddChild("D");
+
+            root.Children[0].AddChild("E").AddChild("F");
+            root.Children[2].AddChild("G").AddChild("H");
+
+            root.Children[0].Children[1].AddChild("I").AddChild("J");
+            root.Children[2].Children[0].AddChild("K");
+
+            return new Dictionary<Node, List<string>>
+            {
+                { root, new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" } },
+            };
+        }
+
+        internal class Node
+        {
+            public string Name { get; set; }
+            public List<Node> Children { get; set; } = new List<Node>();
+
+            public Node(string name)
+            {
+                Name = name;
+            }
+
+            public Node AddChild(string name)
+            {
+                var child = new Node(name);
+                Children.Add(child);
+                return this;
+            }
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_36_KadanesAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/kadane's-algorithm"/>
+    /// Time: O(n) where n is the number of elements in the array
+    /// Space: O(1)
+    internal class MediumQuestion_36_KadanesAlgorithm : CodingQuestion<int[], int>
+    {
+        protected override int ExecuteSolution(int[] array)
+        {
+            var maxEndingHere = array[0];
+            var maxSoFar = array[0];
+
+            // At every element, we calculate the maximum sum of any subarray including the current element,
+            // and we also track the maximum sum of any subarray we saw so far (that will be our result in the end).
+            for (var i = 1; i < array.Length; i++)
+            {
+                // If the current element itself is larger than the sum of any possible subarray before it,
+                // it doesn't worth adding it up to it, rather just take the current element, and dismiss
+                // any elements before it.
+                maxEndingHere = Math.Max(array[i], array[i] + maxEndingHere);
+                maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+            }
+
+            return maxSoFar;
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(int expected, int actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<int[], int> GetTests()
+        {
+            return new Dictionary<int[], int>
+            {
+                {
+                    new int[] { 3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4 },
+                    19
+                }
+            };
+        }
+    }
+}
