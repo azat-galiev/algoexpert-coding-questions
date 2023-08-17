39b123fff8d85e25e28deb6229651ba394b801ff
Azat Galiev <v-azatgaliev@microsoft.com>
2023-08-17 21:58:38 +0200

    Day 37

---
+++ src/DataStructures/MinHeap.cs
@@ -0,0 +1,117 @@
+﻿namespace CodingQuestions.DataStructures
+{
+    /// <see href="https://www.algoexpert.io/questions/min-heap-construction"/>
+    internal class MinHeap
+    {
+        /// <summary>
+        /// Left public for testing purposes.
+        /// </summary>
+        public readonly List<int> heap = new();
+
+        public MinHeap(List<int> list)
+        {
+            heap = list;
+            InitializeHeap();
+        }
+
+        // Time: O(log(n)) where n is the size of the heap
+        // Space: O(1)
+        public void SiftDown(int currentIdx, int endIdx)
+        {
+            // Loop down while we have at least one child.
+            var childOneIdx = currentIdx * 2 + 1;
+            while (childOneIdx <= endIdx)
+            {
+                // Do we have the second child?
+                var childTwoIdx = currentIdx * 2 + 2;
+                if (childTwoIdx > endIdx) childTwoIdx = -1;
+
+                // If we have the second child, and it's smaller than the first one,
+                // go this route, otherwise go towards the first child.
+                var idxToSwap = childOneIdx;
+                if (childTwoIdx != -1 && heap[childTwoIdx] < heap[childOneIdx])
+                    idxToSwap = childTwoIdx;
+
+                // The current element should be lower, so we continue.
+                if (heap[idxToSwap] < heap[currentIdx])
+                {
+                    Swap(idxToSwap, currentIdx, heap);
+
+                    currentIdx = idxToSwap;
+                    childOneIdx = currentIdx * 2 + 1;
+                }
+                // We reached the proper position.
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        // Time: O(log(n)) where n is the size of the heap
+        // Space: O(1)
+        public void SiftUp(int currentIdx)
+        {
+            // In order to sift an element up, we compare it against the parent, 
+            // and if it's smaller (i.e., parent should be lower than the current element), 
+            // we swap them, and continue until some parent is smaller than the current element.
+            var parentIdx = (currentIdx - 1) / 2;
+            while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx])
+            {
+                Swap(currentIdx, parentIdx, heap);
+                currentIdx = parentIdx;
+                parentIdx = (currentIdx - 1) / 2;
+            }
+        }
+
+        // Time: O(1)
+        // Space: O(1)
+        public int Peek()
+        {
+            return heap[0];
+        }
+
+        // Time: O(log(n)) where n is the size of the heap
+        // Space: O(1)
+        public int Remove()
+        {
+            var removed = heap[0];
+
+            // We don't need the first element anymore, so we swap it with the very last element 
+            // (because it's pretty easy to remove the last element) and remove it.
+            Swap(0, heap.Count - 1, heap);
+            heap.RemoveAt(heap.Count - 1);
+
+            // Sift the previously-last element down until it lands a proper position.
+            SiftDown(0, heap.Count - 1);
+
+            return removed;
+        }
+
+        // Time: O(log(n)) where n is the size of the heap
+        // Space: O(1)
+        public void Insert(int value)
+        {
+            // We add the new value to the end of the array (i.e., at the next free position of our BT),
+            // and then sift it up until it lands a correct position.
+            heap.Add(value);
+            SiftUp(heap.Count - 1);
+        }
+
+        // Time: O(n) where n is the size of the array
+        // Space: O(1)
+        private void InitializeHeap()
+        {
+            var firstParentIdx = (heap.Count - 1) / 2;
+            for (var currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--)
+                SiftDown(currentIdx, heap.Count - 1);
+        }
+
+        // Time: O(1)
+        // Space: O(1)
+        private static void Swap(int idx1, int idx2, List<int> heap)
+        {
+            (heap[idx2], heap[idx1]) = (heap[idx1], heap[idx2]);
+        }
+    }
+}
---
+++ src/MediumQuestions/MediumQuestion_52_MinHeap.cs
@@ -0,0 +1,78 @@
+﻿using CodingQuestions.DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingQuestions.MediumQuestions
+{
+    /// <see href="https://www.algoexpert.io/questions/min-heap-construction"/>
+    /// Actual implementation is in <see cref="MinHeap"/>
+    internal class MediumQuestion_52_MinHeap : CodingQuestion<MediumQuestion_52_MinHeap.Input, bool>
+    {
+        protected override bool ExecuteSolution(Input input)
+        {
+            try
+            {
+                input.Test();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        protected override bool CompareActualAndExpectedOuputs(bool expected, bool actual)
+        {
+            return expected == actual;
+        }
+
+        protected override IReadOnlyDictionary<Input, bool> GetTests()
+        {
+            return new Dictionary<Input, bool>
+            {
+                {
+                    new Input
+                    {
+                        Test = () =>
+                        {
+                            var minHeap = new MinHeap(new List<int> { 48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41 });
+                            AssertListsAreEqual(new List<int> { -5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41 }, minHeap.heap);
+
+                            minHeap.Insert(76);
+                            AssertListsAreEqual(new List<int> { -5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76 }, minHeap.heap);
+
+                            Assert.AreEqual(-5, minHeap.Peek());
+                            AssertListsAreEqual(new List<int> { -5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76 }, minHeap.heap);
+
+                            Assert.AreEqual(-5, minHeap.Remove());
+                            AssertListsAreEqual(new List<int> { 2, 7, 6, 24, 8, 8, 24, 391, 76, 56, 12, 24, 48, 41 }, minHeap.heap);
+
+                            Assert.AreEqual(2, minHeap.Peek());
+                            Assert.AreEqual(2, minHeap.Remove());
+                            AssertListsAreEqual(new List<int> { 6, 7, 8, 24, 8, 24, 24, 391, 76, 56, 12, 41, 48 }, minHeap.heap);
+
+                            Assert.AreEqual(6, minHeap.Peek());
+
+                            minHeap.Insert(87);
+                            AssertListsAreEqual(new List<int> { 6, 7, 8, 24, 8, 24, 24, 391, 76, 56, 12, 41, 48, 87 }, minHeap.heap);
+                        }
+                    },
+                    true
+                }
+            };
+        }
+
+        private static void AssertListsAreEqual(List<int> expected, List<int> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        internal class Input
+        {
+            public required Action Test { get; set; }
+        }
+    }
+}
